97324d34523a1b2bc17f51211993fced3637b468
lFrodo <igor@azinet.ru>
2023-03-18 19:15:19 +0300

    +slider +team acco

---
+++ js/accordeon.js
@@ -0,0 +1,34 @@
+const openItem = item =>{
+const container = item.closest('.team__item')
+const contentBlock = container.find('.team__about')
+const textBlock = contentBlock.find('.team__about-block')
+const reqHeight = textBlock.height()
+const descPolygon = container.find('.team__desc')
+container.addClass('active')
+contentBlock.height(reqHeight)
+descPolygon.css('transform','rotate(60deg)')
+
+
+}
+const closeEveryItem = container =>{
+  const items = container.find('.team__about')
+  const itemContainer = container.find('.team__item')
+  const descPolygon = container.find('.team__desc')
+  descPolygon.css('transform','rotate(0deg)')
+  itemContainer.removeClass('active')
+  items.height(0)
+}
+$('.team__name').click(e=>{
+  $this = $(e.currentTarget)
+  const container = $this.closest('.team')
+  const elemContainer = $this.closest('.team__item')
+  if (elemContainer.hasClass('active')){
+    closeEveryItem(container)
+  }else{
+    closeEveryItem(container)
+    openItem($this)
+  
+  }
+
+
+})
